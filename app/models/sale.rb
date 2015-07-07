b68f7d7648b746a66849e8e1184ae598edd8a670
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-06 19:50:02 -0700

    sanitize address and description before saving a sale

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -9,7 +9,13 @@ class Sale < ActiveRecord::Base
   validates :end_time, presence: true
   geocoded_by :address
   after_validation :geocode
-
+  before_save :sanitize_input
   mount_uploader :image_path, ImageUploader
+
+  private
+  def sanitize_input
+    self.address = Rack::Utils.escape_html(self.address) unless self.address.nil?
+    self.description = Rack::Utils.escape_html(self.description) unless self.description.nil?
+  end
 
 end
