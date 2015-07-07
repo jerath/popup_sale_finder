bf65f947aea6861059d5946d8ecdf77fb6058d88
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-06 19:57:55 -0700

    sanitize item name

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ActiveRecord::Base
   belongs_to :sale
   validates :item_name, presence: true
 
+  before_save :sanitize_input
+
+  private
+  def sanitize_input
+    self.item_name = Rack::Utils.escape_html(self.item_name) unless self.item_name.nil?
+  end
 end
